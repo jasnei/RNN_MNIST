1ee6ecd47da36c525cda54d8f30a585a6728cf27
albertyang <albertyang@soundpie.cn>
2021-10-07 09:35:44 +0800

    dataset

--- data.py
+++ data.py
@@ -1,3 +1,5 @@
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 import matplotlib.pyplot as plt
 import torch
 import torch.utils.data as Data
@@ -20,41 +22,35 @@ def get_dataset(root="../data/", train_batch_size=64, test_batch_size=100, num_w
         download=False,
     )
 
-    train_data_x = train_data.data.type(torch.FloatTensor) / 255.0
-    train_data_x = train_data_x.reshape(train_data_x.shape[0], -1)
-    train_data_y = train_data.targets
-
     train_loader = Data.DataLoader(
-        dataset=train_data_x,
+        dataset=train_data,
         batch_size=train_batch_size,
         shuffle=True,
         num_workers=num_workers,
     )
-    test_data_x = test_data.data.type(torch.FloatTensor) / 255.0
-    test_data_x = test_data_x.reshape(test_data_x.shape[0], -1)
-    test_data_y = test_data.targets
 
     test_loader = Data.DataLoader(
-        dataset=test_data_x,
+        dataset=test_data,
         batch_size=test_batch_size,
         shuffle=False,
         num_workers=num_workers,
     )
 
-    print(f"train_data_x.shape: {train_data_x.shape}")
-    print(f"test_data_x.shape: {test_data_x.shape}")
-
-    return train_loader, train_data_y, test_loader, test_data_y
+    return train_loader, test_loader,
 
 if __name__ == '__main__':
 
-    train_loader, train_data_y, test_loader, test_data_y = get_dataset()
+    train_loader, test_loader = get_dataset()
     
-    for step, image in enumerate(train_loader):
-        if step > 0:
-            break
+    print(f"train_loader.len: {len(train_loader)}")
+    print(f"test_loader.len: {len(test_loader)}")
 
-    img = make_grid(image.reshape(-1, 1, 28, 28))
+    for step, (images, labels)in enumerate(train_loader):
+        # print(step)
+        # if step > 0:
+        break
+
+    img = make_grid(images.reshape(-1, 1, 28, 28))
     img = img.data.numpy().transpose(1, 2, 0)
     plt.figure()
     plt.imshow(img, 'gray')
